3fd52bf3dfc10fb0a7679b8a578cdceb5e4a4fcf
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3C-1PGJGP2.dir.svc.accenture.com>
2020-01-29 06:32:46 -0600

    Done

--- src/test/java/TestRunner/RegisterRunner.java
+++ src/test/java/TestRunner/RegisterRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
-(features="FeatureFile",glue= {"StepDefinition"},tags="@login"
+(features="FeatureFile",glue= {"StepDefinition"},tags="@login", plugin= {"pretty","json:target/cucumber.json"}
 		)
 public class RegisterRunner
 {
